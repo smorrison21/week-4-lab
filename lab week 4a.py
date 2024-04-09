8b9ba2a2fb5b36f41fdbe017068474e519188347
smorrison21 <morrisons@uchicago.edu>
2024-04-09 12:20:58 -0500

    Completed

--- lab week 4a.py
+++ lab week 4a.py
@@ -14,9 +14,12 @@ df
 #Try to complete all of the following writing only one line of code each
 
 #1. Select only the GDP columns, for the years 2013-2014.
+df.loc[[2013, 2014], ['gdp_usa', 'gdp_canada', 'gdp_mexico']]
 
 #2. The unemployment rate in the USA in 2014 should be 6.2, not 16.2.
 #   Write one line that replaces that value.
+df.loc[2014, 'unemp_usa'] = 6.2
+df
 
 #3. Calculate two new columns named 'unemp_mean_na' and 'gdp_sum_na'
 #   that are equal to the mean and sum of the respective columns for
@@ -24,14 +27,19 @@ df
 #   (hint: look up the axis key-word argument in the respective
 #   Pandas methods).  You will need one line of code for the sum
 #   and one for the mean.
+df['unemp_mean_na'] = df[['unemp_canada', 'unemp_usa', 'unemp_mexico']].mean(axis = 1)
+df
 
 #4. Display only the values for Canada in 2015 and 2016.
+df.loc[[2015, 2016], [c for c in df.columns if c.endswith('canada')]]
 
 #5. Create a new variable that is a subset of the main data showing
 #   only columns for Mexico from 2014 and later.
+df_mexico = df.loc[[2014, 2015, 2016, 2017], [c for c in df.columns if c.endswith('mexico')]]
 
 #6. Calculate a new column in this subset called 'gdp_delta' which
 #   shows the change in GDP for each country from year to year.  
 #   Hint: you'll need to look up a new Series method for this. All 
 #   the new values will be NaN for the first year.
-
+df_mexico['gdp_delta'] = df_mexico['gdp_mexico'].diff()
+df_mexico
